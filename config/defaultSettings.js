b77efefec66e561c1949a031e90d686359447b11
Bruno Ferreira <brunogonncalves@icloud.com>
2019-08-20 15:03:47 -0300

    initial commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,16 @@
+export default {
+  navTheme: 'dark',
+  primaryColor: '#0a1335',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Instale tecnologia',
+  pwa: false,
+  iconfontUrl: '',
+};
---
+++ config/router.config.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: '/',
+    component: '../layouts/BasicLayout',
+    Routes: ['src/pages/Authorized'],
+    authority: ['admin', 'user'],
+    routes: [
+      {
+        path: '/',
+        name: 'welcome',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+]
