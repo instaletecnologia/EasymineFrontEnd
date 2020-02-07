2cc8f33e80a475ddbbcbedd61de4c38e4e5e9ac2
Bruno <brunogonncalves@icloud.com>
2020-02-07 15:27:14 -0300

    socket service

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,3 +1,23 @@
 import socketIOClient from 'socket.io-client';
 
-export default socketIOClient(window.SOCKET_URL, { autoConnect: true });
+// eslint-disable-next-line no-undef
+export const socketInstance = socketIOClient(SOCKET_URL, {
+  autoConnect: true,
+});
+
+export const EVENTS = {
+  ON_EQUIPMENT_CONNECTION: 'onEquipmentConnection',
+  ON_EQUIPMENT_COMPASS: 'onEquipmentCompass',
+  ON_EQUIPMENT_POSITION: 'onEquipmentPosition',
+  ON_EQUIPMENT_REFRESH_ALL: 'onEquipmentRefreshAll',
+};
+
+socketInstance.on('connect', socket => {
+  console.log('connect');
+});
+
+socketInstance.on('disconnect', () => {
+  console.log('disconnect');
+});
+
+export default socketInstance;
