97b11a522543894e2b028d23dcb8934efd03ffff
Bruno <brunogonncalves@icloud.com>
2020-02-07 15:30:38 -0300

    monitore equipments on map

--- src/modules/Dispatch/controls/GeoProcess/index.js
+++ src/modules/Dispatch/controls/GeoProcess/index.js
@@ -1,13 +1,18 @@
-import React, { memo, useEffect, useState } from 'react';
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable no-use-before-define */
+import React, { memo, useEffect, useMemo, useState } from 'react';
 import _ from 'lodash';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import { Spin } from 'antd';
+import { Marker } from 'react-map-gl';
+import { Spin, notification } from 'antd';
 import proj4 from 'proj4';
 
 import Map from '@/components/Map';
 import Equipment from '@/components/Equipment';
 import styles from './assets/Position.less';
 
+import socket, { EVENTS } from '@/services/socket';
 import { getCoords, getEquipments } from './services/api';
 
 proj4.defs([
@@ -25,37 +30,112 @@ proj4.defs([
 function Position() {
   const [loading, setLoading] = useState(true);
   const [mapCoords, setMapCoords] = useState({ lat: 0, lng: 0 });
-  const [equipments, setEquipments] = useState({});
+  const [equipments, setEquipments] = useState([]);
 
-  const renderEquipmentMarker = equipmentId => {
-    const equipment = equipments[equipmentId];
-    const lat = _.get(equipment, 'UltimaLocalizacao.Latitude');
-    const lng = _.get(equipment, 'UltimaLocalizacao.Longitude');
+  const renderEquipmentMarker = equipment => {
+    const lat = equipment.Latitude || _.get(equipment, 'UltimaLocalizacao.Latitude', mapCoords.lat);
+    const lng =
+      equipment.Longitude || _.get(equipment, 'UltimaLocalizacao.Longitude', mapCoords.lng);
+    const km = equipment.KM || 0;
 
     return (
-      <Marker latitude={lat} longitude={lng} offsetLeft={-20} offsetTop={-10}>
+      <Marker
+        key={`marker-${equipment.EquipamentoID}`}
+        latitude={lat}
+        longitude={lng}
+        km={km}
+        offsetLeft={-20}
+        offsetTop={-10}
+      >
         <Equipment {...equipment} />
       </Marker>
     );
   };
 
   async function loadCoords() {
-    setLoading(true);
-
     const coordsResult = await getCoords();
     setMapCoords(coordsResult);
-
-    setLoading(false);
   }
 
   async function loadEquipments() {
     const equipmentsResult = await getEquipments();
-    setEquipments(equipmentsResult || {});
+    const list = Object.keys(equipmentsResult).map(k => equipmentsResult[k]);
+    setEquipments(list);
+  }
+
+  async function load() {
+    setLoading(true);
+    await Promise.all([loadCoords(), loadEquipments()]);
+    setLoading(false);
+  }
+
+  function onEquipmentConnection({ EquipamentoID, Online }) {
+    console.log('onEquipmentConnection');
+    setEquipments(prevEquipments => {
+      const i = _.findIndex(prevEquipments, { EquipamentoID: parseInt(EquipamentoID, 0) });
+      let newEquipments = prevEquipments;
+      if (i !== -1) {
+        newEquipments[i].Online = Online;
+        const message = `${newEquipments[i].TagPrefixo} ${newEquipments[i].TagNumero}`;
+        const description = `Equipamento ${Online ? 'ONLINE' : 'OFFLINE'}`;
+        const data = { message, description };
+
+        if (Online) notification.success(data);
+        else notification.warning(data);
+
+        return [...newEquipments];
+      }
+      return newEquipments;
+    });
+  }
+
+  function onEquipmentCompass({ EquipamentoID, degree }) {
+    console.log('onEquipmentCompass');
+    setEquipments(prevEquipments => {
+      const i = _.findIndex(prevEquipments, { EquipamentoID: parseInt(EquipamentoID, 0) });
+      let newEquipments = prevEquipments;
+      if (i !== -1) newEquipments[i].Degree = degree;
+      return [...newEquipments];
+    });
+  }
+
+  function onEquipmentPosition({ EquipamentoID, Latitude, Longitude, KM }) {
+    console.log('onEquipmentPosition');
+    setEquipments(prevEquipments => {
+      const i = _.findIndex(prevEquipments, { EquipamentoID: parseInt(EquipamentoID, 0) });
+      let newEquipments = prevEquipments;
+      if (i !== -1) {
+        newEquipments[i].Latitude = Latitude;
+        newEquipments[i].Longitude = Longitude;
+        newEquipments[i].KM = KM;
+        return [...newEquipments];
+      }
+      return newEquipments;
+    });
+  }
+
+  function onRefresh(data) {
+    console.log('onEquipmentRefreshAll');
+    setEquipments(() => [...Object.keys(data).map(k => data[k])]);
+    notification.success({
+      message: 'Monitorando equipamentos',
+    });
   }
 
   useEffect(() => {
-    loadCoords();
-    loadEquipments();
+    load().then(() => {
+      socket.on(EVENTS.ON_EQUIPMENT_CONNECTION, onEquipmentConnection);
+      socket.on(EVENTS.ON_EQUIPMENT_COMPASS, onEquipmentCompass);
+      socket.on(EVENTS.ON_EQUIPMENT_POSITION, onEquipmentPosition);
+      socket.on(EVENTS.ON_EQUIPMENT_REFRESH_ALL, onRefresh);
+    });
+
+    /* return () => {
+      socket.off(EVENTS.ON_EQUIPMENT_CONNECTION)
+      socket.off(EVENTS.ON_EQUIPMENT_COMPASS)
+      socket.off(EVENTS.ON_EQUIPMENT_POSITION)
+      socket.off(EVENTS.ON_EQUIPMENT_REFRESH_ALL)
+    } */
   }, []);
 
   if (loading) return <Spin />;
@@ -63,7 +143,7 @@ function Position() {
   return (
     <div className={styles.container}>
       <Map lat={mapCoords.lat} lng={mapCoords.lng}>
-        {Object.keys(equipments).map(renderEquipmentMarker)}
+        {equipments.map(renderEquipmentMarker)}
       </Map>
     </div>
   );
